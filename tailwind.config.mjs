bc840c198ee9f1dc71162c2502eb166d6cdc9a02
miguel <miguel.teranj02@gmail.com>
2024-12-13 06:28:19 -0400

    finally build vsl section and we're actually using a skeleton for vsl

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,7 +8,7 @@ export default {
       },
       width: {
         'nav-logo': ['clamp(5rem, 12dvw, 6.5rem)'],
-        'hero-foreground': ['clamp(22rem, 50dvw, 30rem)'],  
+        'hero-foreground': ['clamp(24rem, 50dvw, 30rem)'],  
       },
       fontFamily: {
         'alfa-slab-one': ['Alfa Slab One', 'sans-serif'],
@@ -16,6 +16,7 @@ export default {
       },
       fontSize: {
         'lg-title': ['clamp(1.3rem, 2.5dvw, 2rem)'],
+        md: ['clamp(1.25rem, 2.5dvw, 1.35rem)'],
         sm: ['clamp(1rem, 2dvw, 1.250rem)'], 
         xs: ['clamp(0.75rem, 1.5dvw, 0.875rem)'],
       }
