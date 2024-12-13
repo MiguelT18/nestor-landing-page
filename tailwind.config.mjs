44819d336b85fec3a902f4c1ad4912883eee818b
miguel <miguel.teranj02@gmail.com>
2024-12-12 21:14:30 -0400

    add last styles to hero section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,15 +8,15 @@ export default {
       },
       width: {
         'nav-logo': ['clamp(5rem, 12dvw, 6.5rem)'],
-        'hero-foreground': ['clamp(22rem, 50dvw, 26rem)'],  
+        'hero-foreground': ['clamp(22rem, 50dvw, 28rem)'],  
       },
       fontFamily: {
         'alfa-slab-one': ['Alfa Slab One', 'sans-serif'],
         'league-spartan': ['League Spartan Variable', 'sans-serif'],
       },
       fontSize: {
-        'lg-title': ['clamp(1.3rem, 3.8dvw, 1.7rem)'],
-        sm: ['clamp(1rem, 2dvw, 1.150rem)'], 
+        'lg-title': ['clamp(1.3rem, 2dvw, 2rem)'],
+        sm: ['clamp(1rem, 2dvw, 1.250rem)'], 
         xs: ['clamp(0.75rem, 1.5dvw, 0.875rem)'],
       }
     },
